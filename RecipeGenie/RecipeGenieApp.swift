3a87b964c6fd9889ca9855e0959e409c4848d2b1
vineethkanaparthi <vineeth0025@gmail.com>
2023-11-06 16:33:31 -0700

    Initial Commit

---
+++ RecipeGenie/RecipeGenieApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeGenieApp.swift
+//  RecipeGenie
+//
+//  Created by Vineeth Kanaparthi on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeGenieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
