7858272de16344082912101bc97aa14855463ba1
vineethkanaparthi <vineeth0025@gmail.com>
2023-11-20 08:05:42 -0700

    extract sub views i.e., clean code

--- RecipeGenie/Views/IngredientsListView.swift
+++ RecipeGenie/Views/IngredientsListView.swift
@@ -19,55 +19,68 @@ struct IngredientsListView: View {
             Divider()
             
             // MARK: - Title
-            HStack {
-                TitleView(text: "Ingredients: ", textStyle: .title2, color: Constants.titleColor)
-                Spacer()
-                Button(action: {
-                    showIngredients.toggle()
-                }) {
-                    Image(systemName: showIngredients ? "chevron.up.circle" : "chevron.down.circle")
-                        .foregroundColor(Color.blue)
-                        .font(Font.system(size: 24))
-                        .padding(.trailing, 10)
-                }
-            }
-            .accessibilityElement(children: .combine)
-            .accessibility(label: Text("Ingredients"))
-            .accessibility(addTraits: .isHeader)
+            titleDropDownView
             
             Divider()
-            // MARK: - Ingredients List
-            if !ingredientsAndMeasures.isEmpty {
-                if showIngredients {
-                    VStack{
-                        ForEach(ingredientsAndMeasures, id: \.id) { ingredient in
-                            HStack {
-                                Text("\(ingredient.name):")
-                                    .frame(alignment: .leading)
-                                    .font(Font.custom(Constants.customFont, size: UIFont.preferredFont(forTextStyle: .body).pointSize))
-                                    .foregroundColor(Constants.textColor)
-                                    .accessibilityLabel("Ingredient: \(ingredient.name)")
-                                
-                                Spacer()
-                                
-                                Text(ingredient.measure)
-                                    .font(Font.custom(Constants.customFont, size: UIFont.preferredFont(forTextStyle: .body).pointSize))
-                                    .foregroundColor(Constants.textColor)
-                                    .accessibilityLabel("Measurement: \(ingredient.measure)")
-                            }
-                        }
-                    }
-                    .padding(.horizontal)
-                    .padding(.vertical, 5)
+            
+            if showIngredients {
+                if !ingredientsAndMeasures.isEmpty {
+                    // MARK: - Ingredients List
+                    IngredientsView
+                }else{
+                    // MARK: - No Ingredients Message
+                    noIngredientsMessage
                 }
-            }else{
-                // MARK: - No Ingredients Message
-                Text("No ingredients available for this meal.")
-                    .font(Font.custom(Constants.customFont, size: UIFont.preferredFont(forTextStyle: .body).pointSize))
-                    .foregroundColor(Constants.textColor)
-                    .padding()
-                    .accessibilityLabel("No ingredients available")
             }
         }
+    }
+    
+    var titleDropDownView: some View {
+        HStack {
+            TitleView(text: "Ingredients: ", textStyle: .title2, color: Constants.titleColor)
+            Spacer()
+            Button(action: {
+                showIngredients.toggle()
+            }) {
+                Image(systemName: showIngredients ? "chevron.up.circle" : "chevron.down.circle")
+                    .foregroundColor(Color.blue)
+                    .font(Font.system(size: 24))
+                    .padding(.trailing, 10)
+            }
+        }
+        .accessibilityElement(children: .combine)
+        .accessibility(label: Text("Ingredients"))
+        .accessibility(addTraits: .isHeader)
+    }
+    
+    var IngredientsView: some View {
+        VStack{
+            ForEach(ingredientsAndMeasures, id: \.id) { ingredient in
+                HStack {
+                    Text("\(ingredient.name):")
+                        .frame(alignment: .leading)
+                        .font(Font.custom(Constants.customFont, size: UIFont.preferredFont(forTextStyle: .body).pointSize))
+                        .foregroundColor(Constants.textColor)
+                        .accessibilityLabel("Ingredient: \(ingredient.name)")
+                    
+                    Spacer()
+                    
+                    Text(ingredient.measure)
+                        .font(Font.custom(Constants.customFont, size: UIFont.preferredFont(forTextStyle: .body).pointSize))
+                        .foregroundColor(Constants.textColor)
+                        .accessibilityLabel("Measurement: \(ingredient.measure)")
+                }
+            }
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 5)
+    }
+    
+    var noIngredientsMessage: some View {
+        Text("No ingredients available for this meal.")
+            .font(Font.custom(Constants.customFont, size: UIFont.preferredFont(forTextStyle: .body).pointSize))
+            .foregroundColor(Constants.textColor)
+            .padding()
+            .accessibilityLabel("No ingredients available")
     }
 }
